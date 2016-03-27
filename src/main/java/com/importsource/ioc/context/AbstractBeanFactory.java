1af18e9abf50ddf518169454566c72ed012aa06c
Hezf <Hezf@Hezf-PC>
2016-03-27 18:32:13 +0800

    添加注释

--- src/main/java/com/importsource/ioc/context/AbstractBeanFactory.java
+++ src/main/java/com/importsource/ioc/context/AbstractBeanFactory.java
@@ -3,6 +3,11 @@ package com.importsource.ioc.context;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 通用bean工厂。未来可以基于该类添加更多实现。比如基于annotation的实现
+ * @author Hezf
+ *
+ */
 public abstract class AbstractBeanFactory {
 	 protected Map<String, Object> beanMap = new HashMap<String, Object>();
 	 public abstract void init(String xml);
